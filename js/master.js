da36fe1dbe693fdd240546e139c0e96271652143
EthanJamesDodd <EthanJamesDodd@gmail.com>
2018-11-16 10:44:54 -0500

    Vue Events

--- js/master.js
+++ js/master.js
@@ -1,29 +1,55 @@
 (() => {
+    // rework this with a Vue Instance
+    const vm = new Vue({
+        el : "#app",
 
-    function getData() {
-        let targetURL = "./includes/connect.php?modelNo=R58";
+        data : {
+            modelname : "",
+            modelpricing : "",
+            modeldetails : "",
+        },
 
-        fetch(targetURL) //go get the data and bring it back
-        .then(res => res.json()) //Turn the result into a plain JS object
-        .then(data => {
-            console.log(data)
-            //Run a function to parse our data
-            showCarData(data[0]);
-        }) //Lets see what we got
-        .catch(function(error) {
-            console.log(error); //If anything broke, log it into the console
-        });
-    }
+        mounted : function() {
+            console.log("we mounted lads");
 
-    function showCarData(data) {
-        //parse the DB info and put it where it needs to o
-        const {modelName, pricing, modelDetails} = data; //destructuring assignment => MDN JS destructuring
+            document.querySelector('#F55').click();
+        },
 
-        //grab the elements we need, and populate then with data
-        document.querySelector('.modelName').textContent = modelName;
-        document.querySelector('.priceInfo').textContent = `$ ${pricing}.00`;
-        document.querySelector('.modelDetails').textContent = modelDetails;
-    }
+        beforeUpdate : function() {
+            console.log('Things are gonna change...');
+        },
 
-    getData(); //Trigger the getData function
+        updated : function() {
+            console.log('Things are different now');
+        },
+
+        methods : {
+            fetchData(e) {
+                // debugger;
+
+                let targetURL = e.currentTarget.id; // Gets the id of the element via the on click event
+                console.log(e.currentTarget.id);
+
+                fetch(`./includes/connect.php?modelNo=${targetURL}`) //go get the data and bring it back
+                .then(res => res.json()) //Turn the result into a plain JS object
+                .then(data => {
+                    console.log(data)
+                    const {modelName, pricing, modelDetails} = data[0];
+
+                    this.modelname = modelName;
+                    this.modeldetails = modelDetails;
+                    this.modelpricing = pricing;
+                    // showCarData(data[0]);
+                }) //Lets see what we got
+                .catch(function(error) {
+                    console.log(error); //If anything broke, log it into the console
+                });
+                
+            }
+
+        },
+    });
+
+
+    // getData(); //Trigger the getData function
 })();
